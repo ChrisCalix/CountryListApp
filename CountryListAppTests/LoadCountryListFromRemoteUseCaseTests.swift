7f69e9a78b7e660e11ea05e51246c3c2fc6a00ba
christian.sonic <yoh.chrisitan@gmail.com>
2023-11-13 10:00:25 -0400

    refactor in factory pattern to create SUT (system under tests)

--- CountryListAppTests/LoadCountryListFromRemoteUseCaseTests.swift
+++ CountryListAppTests/LoadCountryListFromRemoteUseCaseTests.swift
@@ -11,23 +11,28 @@ import XCTest
 final class LoadCountryListFromRemoteUseCaseTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClientSpy()
-        
-        trackForMemoryLeaks(client)
+        let (_, client) = makeSUT()
         
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
     func test_load_requestsDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteCountryLoader(url: url, client: client)
-        
-        trackForMemoryLeaks(sut)
-        trackForMemoryLeaks(client)
+        let (sut, client) = makeSUT(url: url)
         
         sut.load() { _ in }
         
         XCTAssertEqual(client.requestedURLs, [url])
     }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!, file: StaticString = #file, line: UInt = #line) -> (sut: RemoteCountryLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteCountryLoader(url: url, client: client)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(client, file: file, line: line)
+        return (sut, client)
+    }
+    
 }
