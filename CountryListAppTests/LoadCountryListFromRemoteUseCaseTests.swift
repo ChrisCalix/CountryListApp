2ad1dfb21a844cc82e79d3b333fd3d834dc3a6ff
christian.sonic <yoh.chrisitan@gmail.com>
2023-11-13 10:01:32 -0400

    CountryList/loadTwice/requestsDataFromURLTwice

--- CountryListAppTests/LoadCountryListFromRemoteUseCaseTests.swift
+++ CountryListAppTests/LoadCountryListFromRemoteUseCaseTests.swift
@@ -25,6 +25,16 @@ final class LoadCountryListFromRemoteUseCaseTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url])
     }
     
+    func test_loadTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load() { _ in }
+        sut.load() { _ in }
+        
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!, file: StaticString = #file, line: UInt = #line) -> (sut: RemoteCountryLoader, client: HTTPClientSpy) {
