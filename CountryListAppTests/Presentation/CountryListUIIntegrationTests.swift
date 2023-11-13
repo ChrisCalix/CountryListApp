517cbe0379b0bdeed2e2a81b8fd1bdc91236fc57
christian.sonic <yoh.chrisitan@gmail.com>
2023-11-13 17:35:37 -0400

    testing retry button for image

--- CountryListAppTests/Presentation/CountryListUIIntegrationTests.swift
+++ CountryListAppTests/Presentation/CountryListUIIntegrationTests.swift
@@ -38,33 +38,9 @@ final class CountryListUIIntegrationTests: XCTestCase {
     }
 
     func test_loadFeedCompletion_rendersSuccessfullyLoadedFeed() {
-        let image0 = makeImage(
-            commonName: "a name",
-            officialName: "a name",
-            region: "a region",
-            subregion: "a subregion",
-            png: URL(string: "http://another-url.com")!,
-            svg: URL(string: "http://another-url.com")!,
-            capital: ["a capital"]
-        )
-        let image1 = makeImage(
-            commonName: "a name",
-            officialName: nil,
-            region: "a region",
-            subregion: "a subregion",
-            png: URL(string: "http://another-url.com")!,
-            svg: URL(string: "http://another-url.com")!,
-            capital: ["a capital"]
-        )
-        let image2 = makeImage(
-            commonName: "a name",
-            officialName: nil,
-            region: "a region",
-            subregion: "a subregion",
-            png: URL(string: "http://another-url.com")!,
-            svg: URL(string: "http://another-url.com")!,
-            capital: []
-        )
+        let image0 = makeImage(officialName: "a name", capital: ["a capital"])
+        let image1 = makeImage(officialName: nil, capital: ["a capital"])
+        let image2 = makeImage(officialName: nil, capital: [])
 
         let (sut, loader) = makeSUT()
 
@@ -85,6 +61,210 @@ final class CountryListUIIntegrationTests: XCTestCase {
         XCTAssertEqual(sut.errorMessage, localized("COUNTRY_LIST_CONNECTION_ERROR"))
     }
 
+    func test_feedImageView_loadsImageURLWhenVisible() {
+        let image0 = makeImage(officialName: "a name", imageUrl: URL(string: "http://url-0.com")!, capital: ["a capital"])
+        let image1 = makeImage(officialName: "a name", imageUrl: URL(string: "http://url-1.com")!, capital: ["a capital"])
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        loader.completeCountryListLoading(with: [image0, image1])
+
+        XCTAssertEqual(loader.loadedImageURLs, [], "Expected no image URL requests until views become visible")
+
+        sut.simulateCountryImageViewVisible(at: 0)
+        XCTAssertEqual(loader.loadedImageURLs, [image0.flags.png], "Expected first image URL request once first view becomes visible")
+
+        sut.simulateCountryImageViewVisible(at: 1)
+        XCTAssertEqual(loader.loadedImageURLs, [image0.flags.png, image1.flags.png], "Expected second image URL request once second view also becomes visible")
+    }
+    
+    func test_feedImageView_cancelsImageLoadingWhenNotVisibleAnymore() {
+        let image0 = makeImage(officialName: "a name", imageUrl: URL(string: "http://url-0.com")!, capital: ["a capital"])
+        let image1 = makeImage(officialName: "a name", imageUrl: URL(string: "http://url-1.com")!, capital: ["a capital"])
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        loader.completeCountryListLoading(with: [image0, image1])
+        XCTAssertEqual(loader.cancelledImageURLs, [], "Expected no cancelled image URL requests until image is not visible")
+
+        sut.simulateFeedImageViewNotVisible(at: 0)
+        XCTAssertEqual(loader.cancelledImageURLs, [image0.flags.png], "Expected one cancelled image URL request once first image is not visible anymore")
+
+        sut.simulateFeedImageViewNotVisible(at: 1)
+        XCTAssertEqual(loader.cancelledImageURLs, [image0.flags.png, image1.flags.png], "Expected two cancelled image URL requests once second image is also not visible anymore")
+    }
+    
+    func test_feedImageViewLoadingIndicator_isVisibleWhileLoadingImage() {
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        loader.completeCountryListLoading(with: [makeImage(), makeImage()])
+
+        let view0 = sut.simulateCountryImageViewVisible(at: 0)
+        let view1 = sut.simulateCountryImageViewVisible(at: 1)
+        XCTAssertEqual(view0?.isShowingImageLoadingIndicator, true, "Expected loading indicator for first view while loading first image")
+        XCTAssertEqual(view1?.isShowingImageLoadingIndicator, true, "Expected loading indicator for second view while loading second image")
+
+        loader.completeImageLoading(at: 0)
+        XCTAssertEqual(view0?.isShowingImageLoadingIndicator, false, "Expected no loading indicator for first view once first image loading completes successfully")
+        XCTAssertEqual(view1?.isShowingImageLoadingIndicator, true, "Expected no loading indicator state change for second view once first image loading completes successfully")
+
+        loader.completeImageLoadingWithError(at: 1)
+        XCTAssertEqual(view0?.isShowingImageLoadingIndicator, false, "Expected no loading indicator state change for first view once second image loading completes with error")
+        XCTAssertEqual(view1?.isShowingImageLoadingIndicator, false, "Expected no loading indicator for second view once second image loading completes with error")
+    }
+    
+    func test_feedImageView_rendersImageLoadedFromURL() {
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        loader.completeCountryListLoading(with: [makeImage(), makeImage()])
+
+        let view0 = sut.simulateCountryImageViewVisible(at: 0)
+        let view1 = sut.simulateCountryImageViewVisible(at: 1)
+        XCTAssertEqual(view0?.renderedImage, .none, "Expected no image for first view while loading first image")
+        XCTAssertEqual(view1?.renderedImage, .none, "Expected no image for second view while loading second image")
+
+        let imageData0 = UIImage.make(withColor: .red).pngData()!
+        loader.completeImageLoading(with: imageData0, at: 0)
+        XCTAssertEqual(view0?.renderedImage, imageData0, "Expected image for first view once first image loading completes successfully")
+        XCTAssertEqual(view1?.renderedImage, .none, "Expected no image state change for second view once first image loading completes successfully")
+
+        let imageData1 = UIImage.make(withColor: .blue).pngData()!
+        loader.completeImageLoading(with: imageData1, at: 1)
+        XCTAssertEqual(view0?.renderedImage, imageData0, "Expected no image state change for first view once second image loading completes successfully")
+        XCTAssertEqual(view1?.renderedImage, imageData1, "Expected image for second view once second image loading completes successfully")
+    }
+
+    func test_feedImageViewRetryButton_isVisibleOnImageURLLoadError() {
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        loader.completeCountryListLoading(with: [makeImage(), makeImage()])
+
+        let view0 = sut.simulateCountryImageViewVisible(at: 0)
+        let view1 = sut.simulateCountryImageViewVisible(at: 1)
+        XCTAssertEqual(view0?.isShowingRetryAction, false, "Expected no retry action for first view while loading first image")
+        XCTAssertEqual(view1?.isShowingRetryAction, false, "Expected no retry action for second view while loading second image")
+
+        let imageData = UIImage.make(withColor: .red).pngData()!
+        loader.completeImageLoading(with: imageData, at: 0)
+        XCTAssertEqual(view0?.isShowingRetryAction, false, "Expected no retry action for first view once first image loading completes successfully")
+        XCTAssertEqual(view1?.isShowingRetryAction, false, "Expected no retry action state change for second view once first image loading completes successfully")
+
+        loader.completeImageLoadingWithError(at: 1)
+        XCTAssertEqual(view0?.isShowingRetryAction, false, "Expected no retry action state change for first view once second image loading completes with error")
+        XCTAssertEqual(view1?.isShowingRetryAction, true, "Expected retry action for second view once second image loading completes with error")
+    }
+    
+    func test_feedImageViewRetryButton_isVisibleOnInvalidImageData() {
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        loader.completeCountryListLoading(with: [makeImage()])
+
+        let view = sut.simulateCountryImageViewVisible(at: 0)
+        XCTAssertEqual(view?.isShowingRetryAction, false, "Expected no retry action while loading image")
+
+        let invalidImageData = Data("invalid image data".utf8)
+        loader.completeImageLoading(with: invalidImageData, at: 0)
+        XCTAssertEqual(view?.isShowingRetryAction, true, "Expected retry action once image loading completes with invalid image data")
+    }
+
+    func test_feedImageViewRetryAction_retriesImageLoad() {
+        let image0 = makeImage(imageUrl: URL(string: "http://url-0.com")!)
+        let image1 = makeImage(imageUrl: URL(string: "http://url-1.com")!)
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        loader.completeCountryListLoading(with: [image0, image1])
+
+        let view0 = sut.simulateCountryImageViewVisible(at: 0)
+        let view1 = sut.simulateCountryImageViewVisible(at: 1)
+        XCTAssertEqual(loader.loadedImageURLs, [image0.flags.png, image1.flags.png], "Expected two image URL request for the two visible views")
+
+        loader.completeImageLoadingWithError(at: 0)
+        loader.completeImageLoadingWithError(at: 1)
+        XCTAssertEqual(loader.loadedImageURLs, [image0.flags.png, image1.flags.png], "Expected only two image URL requests before retry action")
+
+        view0?.simulateRetryAction()
+        XCTAssertEqual(loader.loadedImageURLs, [image0.flags.png, image1.flags.png, image0.flags.png], "Expected third imageURL request after first view retry action")
+
+        view1?.simulateRetryAction()
+        XCTAssertEqual(loader.loadedImageURLs, [image0.flags.png, image1.flags.png, image0.flags.png, image1.flags.png], "Expected fourth imageURL request after second view retry action")
+    }
+
+    func test_feedImageView_preloadsImageURLWhenNearVisible() {
+        let image0 = makeImage(imageUrl: URL(string: "http://url-0.com")!)
+        let image1 = makeImage(imageUrl: URL(string: "http://url-1.com")!)
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        loader.completeCountryListLoading(with: [image0, image1])
+        XCTAssertEqual(loader.loadedImageURLs, [], "Expected no image URL requests until image is near visible")
+
+        sut.simulateFeedImageViewNearVisible(at: 0)
+        XCTAssertEqual(loader.loadedImageURLs, [image0.flags.png], "Expected first image URL request once first image is near visible")
+
+        sut.simulateFeedImageViewNearVisible(at: 1)
+        XCTAssertEqual(loader.loadedImageURLs, [image0.flags.png, image1.flags.png], "Expected second image URL request once second image is near visible")
+    }
+
+    func test_feedImageView_cancelsImageURLPreloadingWhenNotNearVisibleAnymore() {
+        let image0 = makeImage(imageUrl: URL(string: "http://url-0.com")!)
+        let image1 = makeImage(imageUrl: URL(string: "http://url-1.com")!)
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        loader.completeCountryListLoading(with: [image0, image1])
+        XCTAssertEqual(loader.cancelledImageURLs, [], "Expected no cancelled image URL requests until image is not near visible")
+
+        sut.simulateFeedImageViewNotNearVisible(at: 0)
+        XCTAssertEqual(loader.cancelledImageURLs, [image0.flags.png], "Expected first cancelled image URL request once first image is not near visible anymore")
+
+        sut.simulateFeedImageViewNotNearVisible(at: 1)
+        XCTAssertEqual(loader.cancelledImageURLs, [image0.flags.png, image1.flags.png], "Expected second cancelled image URL request once second image is not near visible anymore")
+    }
+
+    func test_feedImageView_doesNotRenderLoadedImageWhenNotVisibleAnymore() {
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+        loader.completeCountryListLoading(with: [makeImage()])
+
+        let view = sut.simulateFeedImageViewNotVisible(at: 0)
+        loader.completeImageLoading(with: anyImageData())
+
+        XCTAssertNil(view?.renderedImage, "Expected no rendered image when an image load finishes after the view is not visible anymore")
+    }
+
+    func test_loadFeedCompletion_dispatchesFromBackgroundToMainThread() {
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+
+        let exp = expectation(description: "Wait for background queue")
+        DispatchQueue.global().async {
+            loader.completeCountryListLoading(at: 0)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+
+    func test_loadImageDataCompletion_dispatchesFromBackgroundToMainThread() {
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        loader.completeCountryListLoading(with: [makeImage()])
+        _ = sut.simulateCountryImageViewVisible(at: 0)
+
+        let exp = expectation(description: "Wait for background queue")
+        DispatchQueue.global().async {
+            loader.completeImageLoading(with: self.anyImageData(), at: 0)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+
+    
     // MARK: - Helpers
 
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: CountryListViewController, loader: LoaderSpy) {
@@ -96,25 +276,25 @@ final class CountryListUIIntegrationTests: XCTestCase {
     }
 
     private func makeImage(
-        commonName: String,
         officialName: String? = nil,
-        region: String,
-        subregion: String,
-        timezones: [String] = [],
-        png: URL = anyURL(),
-        svg: URL = anyURL(),
-        flagURL: URL = anyURL(),
-        continents: [String] = [],
-        capital: [String]
+        imageUrl: URL = URL(string: "http://another-url.com")!,
+        capital: [String] = []
     ) -> CountryListItem {
         return CountryListItem(
-            name: CountryListItem.Name(common: commonName, official: officialName),
-            region: region,
-            subregion: subregion,
-            flags: CountryListItem.flagsImage(png: png, svg: svg),
+            name: CountryListItem.Name(common: "a name", official: officialName),
+            region: "a region",
+            subregion: "a subregion",
+            flags: CountryListItem.flagsImage(
+                png: imageUrl,
+                svg: imageUrl
+            ),
             capital: capital,
-            timezones: timezones,
-            continents: continents
+            timezones: [],
+            continents: []
         )
+    }
+    
+    private func anyImageData() -> Data {
+        return UIImage.make(withColor: .red).pngData()!
     }
 }
