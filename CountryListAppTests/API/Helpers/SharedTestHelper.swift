b7a650a14e0a9d02449cb0adbe3e292275103a4e
christian.sonic <yoh.chrisitan@gmail.com>
2023-11-13 11:27:44 -0400

    imageData/loadFromURL/doesNotDeliverResultAfterCancellingTask

--- CountryListAppTests/API/Helpers/SharedTestHelper.swift
+++ CountryListAppTests/API/Helpers/SharedTestHelper.swift
@@ -11,6 +11,10 @@ func anyURL() -> URL {
     return URL(string: "https://any-url.com")!
 }
 
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
+
 func anyData() -> Data {
     return Data("any data".utf8)
 }
