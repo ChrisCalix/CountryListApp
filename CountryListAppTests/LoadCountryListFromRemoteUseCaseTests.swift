01c3c2d99fade992b0b690e40d06ded4bf5abccf
christian.sonic <yoh.chrisitan@gmail.com>
2023-11-13 10:08:45 -0400

    countryList/load/deliversErrorOnNon200HTTPResponse

--- CountryListAppTests/LoadCountryListFromRemoteUseCaseTests.swift
+++ CountryListAppTests/LoadCountryListFromRemoteUseCaseTests.swift
@@ -58,6 +58,33 @@ final class LoadCountryListFromRemoteUseCaseTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
+    func test_load_deliversErrorOnNon200HTTPResponse() {
+        let (sut, client) = makeSUT()
+        
+        let samples = [199, 201, 300, 400, 500]
+        
+        samples.enumerated().forEach { index, code in
+            let expectedResult: RemoteCountryLoader.Result = failure(.invalidData)
+            let exp = expectation(description: "Wait for load")
+            
+            sut.load { receivedResult in
+                switch ( receivedResult, expectedResult) {
+                
+                case let (.failure(receivedError as RemoteCountryLoader.Error), .failure(expectedError as RemoteCountryLoader.Error)):
+                    XCTAssertEqual(receivedError, expectedError)
+                default:
+                    XCTFail("Expected result \(expectedResult) instead")
+                }
+                exp.fulfill()
+            }
+            
+            let json = makeItemsJSON([])
+            client.complete(withStatusCode: code, data: json, at: index)
+            
+            wait(for: [exp], timeout: 1.0)
+        }
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!, file: StaticString = #file, line: UInt = #line) -> (sut: RemoteCountryLoader, client: HTTPClientSpy) {
@@ -70,5 +97,10 @@ final class LoadCountryListFromRemoteUseCaseTests: XCTestCase {
     
     private func failure(_ error: RemoteCountryLoader.Error) -> RemoteCountryLoader.Result {
         return .failure(error)
+    }
+    
+    private func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+        let json = items
+        return try! JSONSerialization.data(withJSONObject: json)
     }
 }
