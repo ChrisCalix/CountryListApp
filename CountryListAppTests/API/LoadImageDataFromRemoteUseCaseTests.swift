39e25e3bc14ab41cf90924c0de7a0bc03f903e6b
christian.sonic <yoh.chrisitan@gmail.com>
2023-11-13 11:08:55 -0400

    refactor makeSUT in factory method

--- CountryListAppTests/API/LoadImageDataFromRemoteUseCaseTests.swift
+++ CountryListAppTests/API/LoadImageDataFromRemoteUseCaseTests.swift
@@ -11,23 +11,27 @@ import XCTest
 final class LoadImageDataFromRemoteUseCaseTests: XCTestCase {
     
     func test_init_doesNotPerformAnyUrlRequest() {
-        let client = HTTPClientSpy()
-        
-        trackForMemoryLeaks(client)
+        let (_, client) = makeSUT()
         
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
     func test_loadImageDataFromURL_requestsDataFromURL() {
         let url = URL(string: "https://a-given0url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteImageDataLoader(client: client)
-        
-        trackForMemoryLeaks(client)
-        trackForMemoryLeaks(sut)
+        let (sut, client) = makeSUT(url: url)
         
         _ = sut.loadImageData(from: url) { _ in }
         
         XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(url: URL = anyURL(), file: StaticString = #file, line: UInt = #line) -> (sut: RemoteImageDataLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteImageDataLoader(client: client)
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, client)
     }
 }
